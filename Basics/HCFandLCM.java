e69f7186f00b08a1ed75a533cf39fd13744a9f54
sumitsingh190 <achievingprinceraj@gmail.com>
2025-06-08 04:54:23 +0530

    HCF and LCM of two numbers

---
+++ Basics/HCFandLCM.java
@@ -0,0 +1,31 @@
+package Basics;
+
+public class HCFandLCM {
+   
+    // Function to find HCF using Euclidean algorithm
+    public static int findHCF(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;  // Now, a is the HCF
+    }
+
+    // Function to find LCM using the relation: LCM(a, b) = (a * b) / HCF(a, b)
+    public static int findLCM(int a, int b) {
+        return (a * b) / findHCF(a, b);
+    }
+
+    public static void main(String[] args) {
+        int num1 = 60, num2 = 48;
+
+        int hcf = findHCF(num1, num2);
+        int lcm = findLCM(num1, num2);
+
+        System.out.println("HCF of " + num1 + " and " + num2 + " = " + hcf);
+        System.out.println("LCM of " + num1 + " and " + num2 + " = " + lcm);
+    }
+}
+//Time complexity: O(log(min(a, b)))
+//Space Complexity: O(1)
